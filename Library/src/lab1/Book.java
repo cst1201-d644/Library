ead797ed57e265a8a96a0f082fb9482a28572eed
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-02-17 20:11:58 -0500

    Some enhancements and javadoc.

--- Library/src/lab1/Book.java
+++ Library/src/lab1/Book.java
@@ -8,53 +8,65 @@ package lab1;
  */
 public class Book {
 
-    private String title;
+	/**
+	 * This book's title.
+	 */
+	private final String title;
 
-    private boolean borrowed;
+	/**
+	 * True if this book is checked out and false otherwise.
+	 */
+	private boolean borrowed;
 
-    /**
-     * Creates a new Book
-     */
-    public Book(String bookTitle) {
-        this.title = bookTitle;
-    }
+	/**
+	 * Creates a new Book
+	 *
+	 * @param bookTitle
+	 */
+	public Book(String bookTitle) {
+		this.title = bookTitle;
+	}
 
-    /**
-     * Marks the book as rented
-     */
-    public void rented() {
-        this.borrowed = true;
-    }
+	/**
+	 * Marks the book as rented
+	 */
+	public void rented() {
+		this.borrowed = true;
+	}
 
-    /**
-     * Marks the book as not rented
-     */
-    public void returned() {
-        this.borrowed = false;
-    }
+	/**
+	 * Marks the book as not rented
+	 */
+	public void returned() {
+		this.borrowed = false;
+	}
 
-    /**
-     * Returns true if the book is rented, false otherwise
-     */
-    public boolean isBorrowed() {
-        return this.borrowed;
-    }
+	/**
+	 * Returns true if the book is rented, false otherwise.
+	 *
+	 * @return True if this book is rented and false otherwise.
+	 */
+	public boolean isBorrowed() {
+		return this.borrowed;
+	}
 
-    /**
-     * Returns the title of the book
-     */
-    public String getTitle() {
-        return this.title;
-    }
+	/**
+	 * Returns the title of this book.
+	 *
+	 * @return This book's title.
+	 */
+	public String getTitle() {
+		return this.title;
+	}
 
-    public static void main(String[] arguments) {
-        // Small test of the Book class
-        Book example = new Book("The Da Vinci Code");
-        System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
-        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-        example.rented();
-        System.out.println("Borrowed? (should be true): " + example.isBorrowed());
-        example.returned();
-        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-    }
+	public static void main(String[] arguments) {
+		// Small test of the Book class
+		Book example = new Book("The Da Vinci Code");
+		System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
+		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+		example.rented();
+		System.out.println("Borrowed? (should be true): " + example.isBorrowed());
+		example.returned();
+		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+	}
 }
